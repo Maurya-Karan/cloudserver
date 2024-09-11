b5a169159a3aa8dbd03d37055a8c3eda0ba5e548
Maurya-Karan <karanmaurya81002@gmail.com>
2024-09-11 20:16:38 +0530

    cors error

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 3000;
 
 app.use(
     cors({
-      origin: "*" || "https://f6c6-174-129-135-255.ngrok-free.app/",
+      origin: '*',
       methods: ["GET", "POST"],
       credentials: true,
     })
